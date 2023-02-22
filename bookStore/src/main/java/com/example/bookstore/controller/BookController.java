954a02a2e4ccab3e8ae6622ed3c992babc2cef27
Thomas <Jia.Liang.Guo@ocean.ibm.com>
2023-02-22 14:31:51 +0800

    Add hystrix-dashboard

--- bookStore/src/main/java/com/example/bookstore/controller/BookController.java
+++ bookStore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,8 +1,10 @@
 package com.example.bookstore.controller;
 
 import com.example.bookstore.feign.WareFeign;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,20 @@ public class BookController {
         log.info("get books from book ware house");
         wareFeign.getBook();
     }
+
+    @GetMapping("/getBook/{id}")
+    @HystrixCommand(fallbackMethod = "hystrixGet")
+    public void getBookById(@PathVariable("id") String id) {
+        log.info("输入的id " + id);
+        if (Integer.valueOf(id) <= 0) {
+            throw new RuntimeException("id " + id + "不存在");
+        }
+    }
+
+    public void hystrixGet(@PathVariable("id") String id) {
+        log.info("输入的id无效");
+    }
+
 
     @GetMapping("/healthz")
     public String healthz() {
